ef0b8fd31d3167ad7193d6bab190f4c77041eda7
Paul Thiberville <paul.thiberville@live.fr>
2022-08-30 20:56:59 +0200

    customFetch and dashboard changes

---
+++ src/utils/customFetch.js
@@ -0,0 +1,26 @@
+export const customFetch = async ({ route, verb, data, token }) => {
+  //const baseUrl = process.env.REACT_APP_API_URL;
+  const baseUrl = "http://localhost:5000";
+  let output = { status: "", data: {} };
+  try {
+    const options = {
+      method: verb || "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (token) options.headers.Authorization = "Bearer " + token;
+    if (data) options.body = JSON.stringify(data);
+    const response = await fetch(baseUrl + route, options);
+    output = { status: response.status, data: await response.json() };
+
+    if (output.data.error) throw new Error(output.data.error);
+
+    return output;
+  } catch (error) {
+    alert("Error on custom fetch : " + (error.message || "unknown error"));
+    return output;
+  }
+};
